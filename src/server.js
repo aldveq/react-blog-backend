4de7bcdb6d2905033fd458cfa1c08da996f1f178
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2022-08-29 18:18:50 -0600

    added comment functionality

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ import bodyParser from 'body-parser';
 const postsData = {
 	'learn-react': {
 		upvotes: 0,
+		comments: [],
 	},
 	'learn-node': {
 		upvotes: 0,
+		comments: [],
 	},
 	'learn-javascript': {
 		upvotes: 0,
+		comments: [],
 	}
 };
 
@@ -17,11 +20,23 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Upvote endpoint
 app.post('/api/posts/:name/upvote', (req, res) => {
 	const postName = req.params.name;
 	
 	postsData[postName].upvotes += 1;
 	res.status(200).send(`Post ${postName} now has ${postsData[postName].upvotes} votes!`);
+});
+
+// Add comment endpoint
+app.post('/api/posts/:name/add-comment', (req, res) => {
+	const postName = req.params.name;
+	const { user, comment } = req.body;
+
+	postsData[postName].comments.push({user, comment});
+
+	res.status(200).send(postsData[postName]);
+
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
