922764100fca88d919ce2ef8a945862a4fe7e6ff
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2022-08-30 21:42:11 -0600

    added mongodb js library + added post data endpoint

--- src/server.js
+++ src/server.js
@@ -1,24 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-const postsData = {
-	'learn-react': {
-		upvotes: 0,
-		comments: [],
-	},
-	'learn-node': {
-		upvotes: 0,
-		comments: [],
-	},
-	'learn-javascript': {
-		upvotes: 0,
-		comments: [],
-	}
-};
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(bodyParser.json());
+
+// Get post data by name
+app.get('/api/posts/:name', async (req, res) => {
+	try {
+		const postName = req.params.name;
+
+		const mongoClient = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+		const db = mongoClient.db('blog-db');
+		const postData = await db.collection('posts').findOne({ name: postName });
+
+		//Sending response back
+		res.status(200).json(postData);
+
+		mongoClient.close();
+	} catch (error) {
+		res.status(500).json({ message: 'Erros connecting to db', error });
+	}
+});
 
 // Upvote endpoint
 app.post('/api/posts/:name/upvote', (req, res) => {
